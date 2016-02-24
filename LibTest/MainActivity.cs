c1ea61a59c8cae5b7762ddcba5627f2c74d86fc2
Ivan Rodrigo Toledo Ivanovic <ivantoledo2002@hotmail.com>
2016-02-24 13:29:09 -0300

    primer commit funcionando

---
+++ LibTest/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using CL.Birdie.Demolibreria;
+
+namespace LibTest
+{
+    [Activity(Label = "LibTest", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            //button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button.Click += (sender, args) =>
+            {
+                var i = new Intent(this.ApplicationContext, typeof(LibraryActivity));
+                StartActivity(i);
+                //var i = new Intent(this.ApplicationContext, LibraryActivity.class)
+                //;
+
+            };
+        }
+    }
+}
+
